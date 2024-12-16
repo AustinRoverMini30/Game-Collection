750b5ae75cebd37a92712a43a2be1f03d7f20779
AustinRoverMini30 <114686049+AustinRoverMini30@users.noreply.github.com>
2024-12-16 17:14:53 +0100

    Mise à jour des controllers et models

--- controllers/controllerChoice.php
+++ controllers/controllerChoice.php
@@ -1,9 +1,34 @@
 <?php
 
-if (!isset($_GET["page"]) || $_GET["page"] == "accueil") {
-    include 'controllerAccueil.php';
-}
-else {
+if (!isset($_GET["page"])) {
+    if ($_GET["page"] == "accueil") {
+        include 'controllerAccueil.php';
+    }
+    else if ($_GET["page"] == "ajout") {
+        include 'controllerAjout.php';
+    }
+    else if ($_GET["page"] == "formulaireAjout") {
+        include 'controllerAjoutFormulaire.php';
+    }
+    else if ($_GET["page"] == "classement") {
+        include 'controllerClassement.php';
+    }
+    else if ($_GET["page"] == "connexion") {
+        include 'controllerConnexion.php';
+    }
+    else if ($_GET["page"] == "formulaire") {
+        include 'controllerFormulaire.php';
+    }
+    else if ($_GET["page"] == "inscription") {
+        include 'controllerInscription.php';
+    }
+    else if ($_GET["page"] == "modificationJeu") {
+        include 'controllerModificationJeu.php';
+    }
+    else if ($_GET["page"] == "profil") {
+        include 'controllerProfil.php';
+    }
+} else {
     include 'controllerAccueil.php';
 }
 
