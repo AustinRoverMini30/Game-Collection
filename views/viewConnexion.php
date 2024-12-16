68697d61b9b3a1a82a53065526b1a9f7d427dd0e
Justine2805 <justine@info-tec.fr>
2024-12-16 15:44:34 +0100

    Ajout du footer dans viewConnexion.php à tester

---
+++ views/viewConnexion.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Footer</title>
+  <link rel="stylesheet" href="../assets/css/style.css">
+</head>
+<body>
+  <footer>
+    Game Collection - 2024 - Tous droits réservés
+  </footer>
+</body>
+</html>
