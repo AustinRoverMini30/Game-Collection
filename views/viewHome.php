b22a5a33d6e5358f3ffbd061bf67f4e22aad0296
noctandc <maloucharan@gmail.com>
2025-01-08 20:17:05 +0100

    correction de lien

--- views/viewHome.php
+++ views/viewHome.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/css/styleGlobal.css">
-    <link rel="stylesheet" href="../assets/css/styleHome.css">
+    <link rel="stylesheet" href="./assets/css/styleGlobal.css">
+    <link rel="stylesheet" href="./assets/css/styleHome.css">
     <title>Home</title>
 </head>
 <body>
@@ -14,14 +14,14 @@
 
     <main>
         <div id="banniereSalutation">
-            <img id="imgBackground" src="../assets/images/banniereExemple.jpeg" alt="image rectangulaire prenant toute la largeur de l'écran"/>
+            <img id="imgBackground" src="./assets/images/banniereExemple.jpeg" alt="image rectangulaire prenant toute la largeur de l'écran"/>
             <h1 id="l1BS">SALUT PRENOM !<br>PRÊT A AJOUTER DES<br>JEUX A TA COLLECTION ?</h1>
         </div>
 
         <div id="mesJeux">
             <h2>Mes jeux</h2>
             <div class="jeu">
-                <img class="jeuBackground" src="../assets/images/minecraftAffiche.jpg" alt="couverture deMinecraft"/> <br>
+                <img class="jeuBackground" src="./assets/images/minecraftAffiche.jpg" alt="couverture deMinecraft"/> <br>
                 <div class="jeuInfo">
                     <h1 class="nomJeu">Minecraft</h1>
                     <p class="heureJouees">120 heures</p> <br>
