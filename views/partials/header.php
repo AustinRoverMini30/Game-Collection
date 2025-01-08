b3fc1ad08852c31e9b586b9095701c9be637ef94
AustinRoverMini30 <114686049+AustinRoverMini30@users.noreply.github.com>
2025-01-08 19:01:39 +0100

    Fichiers dédiés à la navbar et au footer.

---
+++ views/partials/header.php
@@ -0,0 +1,14 @@
+<header>
+    <div class="container">
+        <div id="logo">
+            <a href="accueil"><img id="logoDuSite" alt="Logo du site web" src="./assets/images/logo.png"></a>
+        </div>
+        <div id="navigation">
+            <a href="viewBibliotheque.php"><button id="maBibliotheque">MA BIBLIOTHEQUE</button></a>
+            <a href="viewAjout.php"><button id="ajouter1Jeu">AJOUTER UN JEU</button></a>
+            <a href="viewClassements.php"><button id="classemenT">CLASSEMENT</button></a>
+            <a href="viewProfil.php"><button id="profil">PROFIL</button></a> 
+        </div>
+
+    </div>
+    </header>
--- views/viewHome.php
+++ views/viewHome.php
@@ -8,20 +8,8 @@
 </head>
 <body>
     <!-- HEADER -->
-    <header>
-    <div class="container">
-        <div id="logo">
-            <a href="accueil"><img id="logoDuSite" alt="Logo du site web" src="./assets/images/logo.png"></a>
-        </div>
-        <div id="navigation">
-            <a href="viewBibliotheque.php"><button id="maBibliotheque">MA BIBLIOTHEQUE</button></a>
-            <a href="viewAjout.php"><button id="ajouter1Jeu">AJOUTER UN JEU</button></a>
-            <a href="viewClassements.php"><button id="classemenT">CLASSEMENT</button></a>
-            <a href="viewProfil.php"><button id="profil">PROFIL</button></a> 
-        </div>
 
-    </div>
-    </header>
+    <?php include './views/partials/header.php'; ?>
 
     <main>
         <div id="banniereSalutation">
@@ -51,8 +39,6 @@
     </main>
 
     <!-- FOOTER -->
-    <footer>
-        <p>Game Collection - 2024 - Tous droits réservés</p>
-    </footer>
+    <?php include './views/partials/footer.php'; ?>
 </body>
 </html>
