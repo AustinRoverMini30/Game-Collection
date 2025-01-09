348be70bd2e6427296eecb39c5500fe20ee25e48
AustinRoverMini30 <114686049+AustinRoverMini30@users.noreply.github.com>
2025-01-09 14:58:00 +0100

    Préparation des pages

--- views/viewModificationJeu.php
+++ views/viewModificationJeu.php
@@ -1,14 +1,22 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Footer</title>
-  <link rel="stylesheet" href="../assets/css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport"content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/styleGlobal.css">
+    <title>Home</title>
 </head>
 <body>
-  <footer>
-    Game Collection - 2024 - Tous droits réservés
-  </footer>
+    <!-- HEADER -->
+
+    <?php include './views/partials/header.php'; ?>
+
+    <main>
+
+        <!-- FOOTER -->
+        <?php include './views/partials/footer.php'; ?>
+
+    </main>
+
 </body>
 </html>
---
+++ views/viewProfil.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/styleGlobal.css">
+    <title>Home</title>
+</head>
+<body>
+    <!-- HEADER -->
+
+    <?php include './views/partials/header.php'; ?>
+
+    <main>
+
+        <!-- FOOTER -->
+        <?php include './views/partials/footer.php'; ?>
+
+    </main>
+
+</body>
+</html>
