a7e6c3cba5c5a93899d0bb47f5573c08b628d6d7
AustinRoverMini30 <114686049+AustinRoverMini30@users.noreply.github.com>
2025-01-11 00:36:33 +0100

    Modification de la page connexion

--- views/viewConnexion.php
+++ views/viewConnexion.php
@@ -1,27 +1,33 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
+<l>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Footer</title>
-  <link rel="stylesheet" href="../assets/css/style.css">
+  <link rel="stylesheet" href="../assets/css/styleGlobal.css">
+  <link rel="stylesheet" href="../assets/css/styleConnexion.css">
 </head>
 <body class="connexion">
+
+    <?php include 'partials/header.php'; ?>
+
     <main>
         <div class="form-container">
-            <h1>Se connecter à Game Collection</h1>
-            <form action="connexion" method="POST">
+            <h2>Se connecter à Game Collection</h2>
+            <form action="connexion" method="POST" id="formulaireConnexion">
                 <label for="email_util">Email :</label>
-                <input type="email" id="email_util" name="email_util" required>
+                <input type="email" id="email_util" name="email_util" required class="inputText">
 
                 <label for="password_util">Mot de passe :</label>
-                <input type="password" id="password_util" name="password_util" required>
+                <input type="password" id="password_util" name="password_util" required class="inputText">
 
-                <button type="submit">SE CONNECTER</button>
+                <button type="submit" id="buttonConnexion">SE CONNECTER</button>
 
-                <a href="inscription">S'inscrire</a>
+                <a href="inscription" id="inscription">S'inscrire</a>
             </form>
         </div> 
+
+        <?php include 'partials/footer.php'; ?>
     </main>
 </body>
 </html>
