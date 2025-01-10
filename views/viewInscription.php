a21a9c10f940ad09c76e7b712f02322f8e165608
AustinRoverMini30 <114686049+AustinRoverMini30@users.noreply.github.com>
2025-01-11 00:43:24 +0100

    Modification du style des pages connexion et inscription

--- views/viewInscription.php
+++ views/viewInscription.php
@@ -4,31 +4,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inscription</title>
-    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/styleGlobal.css">
+    <link rel="stylesheet" href="./assets/css/styleInscription.css">
 </head>
 <body>
-    <div class="form-container">
-        <h1>Inscription</h1>
-        <form action="inscription" method="POST">
-            <label for="nom">Nom :</label>
-            <input type="text" id="nom" name="nom" required>
 
-            <label for="prenom">Prénom :</label>
-            <input type="text" id="prenom" name="prenom" required>
+    <?php include './views/partials/header.php'; ?>
 
-            <label for="email">Email :</label>
-            <input type="email" id="email" name="email" required>
+    <main>
+        <div class="form-container">
+            <h1>Inscription</h1>
+            <form action="inscription" method="POST">
+                <label for="nom">Nom :</label>
+                <input type="text" id="nom" name="nom" required>
 
-            <label for="password">Mot de passe :</label>
-            <input type="password" id="password" name="password" required>
+                <label for="prenom">Prénom :</label>
+                <input type="text" id="prenom" name="prenom" required>
 
-            <label for="confirm_password">Confirmation du mot de passe :</label>
-            <input type="password" id="confirm_password" name="confirm_password" required>
+                <label for="email">Email :</label>
+                <input type="email" id="email" name="email" required>
 
-            <button type="submit">S'INSCRIRE</button>
+                <label for="password">Mot de passe :</label>
+                <input type="password" id="password" name="password" required>
 
-            <a href="connexion">Se connecter</a>
-        </form>
-    </div>
+                <label for="confirm_password">Confirmation du mot de passe :</label>
+                <input type="password" id="confirm_password" name="confirm_password" required>
+
+                <button type="submit">S'INSCRIRE</button>
+
+                <a href="connexion">Se connecter</a>
+            </form>
+        </div>
+
+        <?php include './views/partials/footer.php'; ?>
+    </main>
 </body>
 </html>
