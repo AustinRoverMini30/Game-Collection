fb16d98567e3c6cab5b335cfe90fa9c14eaf71e7
AustinRoverMini30 <114686049+AustinRoverMini30@users.noreply.github.com>
2025-01-11 12:29:49 +0100

    Modification de la page profil

--- models/modelProfil.php
+++ models/modelProfil.php
@@ -1,6 +1,47 @@
 <?php
 
 class ModelProfil {
+
+    private $pdo;
+
+    public function __construct($pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+
+    public function deleteUser($id)
+    {
+        $stmt = $this->pdo->prepare("DELETE FROM UTILISATEUR WHERE id_util = :id");
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+    }
+
+    public function updateUser($userId, $nom, $prenom, $mail)
+    {
+        $stmt = $this->pdo->prepare("UPDATE UTILISATEUR SET nom_util = :nom, prenom_util = :prenom, email_util = :mail WHERE id_util = :userId");
+        $stmt->bindParam(':userId', $userId);
+        $stmt->bindParam(':nom', $nom);
+        $stmt->bindParam(':prenom', $prenom);
+        $stmt->bindParam(':mail', $mail);
+        $stmt->execute();
+    }
+
+    public function updatePwd($userId, $pwd)
+    {
+        $stmt = $this->pdo->prepare("UPDATE UTILISATEUR SET password_util = :pwd WHERE id_util = :userId");
+        $stmt->bindParam(':userId', $userId);
+        $stmt->bindParam(':pwd', password_hash($pwd, PASSWORD_BCRYPT));
+        $stmt->execute();
+    }
+
+    public function getUser($id)
+    {
+        $stmt = $this->pdo->prepare("SELECT * FROM UTILISATEUR WHERE id_util = :id");
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
     
 }
 
--- views/viewProfil.php
+++ views/viewProfil.php
@@ -14,55 +14,21 @@
 
     <main>
 
-        <?php 
-        if(isset($_POST["modifierProfil"])){
-            echo '<div class="clause-profil">
-            <h1>Mon profil</h1>
+        <div class=clause-profil>
+            <h2>Mon profil</h2>
+
+            <h3>Nom : <?php echo $nom ?></h3>
+            <h3>Prénom : <?php echo $prenom ?></h3>
+            <h3>Email : <?php echo $mail ?></h3>
+            
             <form method="POST">
-                <label for="nom_util">Nom :</label> <br>
-                <input type="text" id="nom_util" name="nom_util" required> <br><br>
-
-                <label for="prenom_util">Prénom :</label>  <br>
-                <input type="text" id="prenom_util" name="prenom_util" required> <br><br>
-
-                <label for="email_util">Email :</label>  <br>
-                <input type="email" id="email_util" name="email_util" required> <br><br>
-
-                <label for="password_util">Mot de passe :</label>  <br>
-                <input type="password" id="password_util" name="password_util" required>  <br> <br>
-
-                <label for="confirm_password">Confirmation du mot de passe :</label>  <br>
-                <input type="password" id="confirm_password" name="confirm_password" required>  <br> <br>
-                 <br>
-                <form method="POST">
-                    <button type="submit" name="validerModifier">MODIFIER</button>
-                </form>
+            <button type="submit" name="modifierProfil">MODIFIER MON PROFIL</button>
+            <button type="submit" name="supprimerProfil">SUPPRIMER MON COMPTE</button>
+            <button type="submit" name="deconnexionProfil">SE DECONNECTER</button>
             </form>
-        </div>';
-        } elseif (isset($_POST["validerModifier"])){
-            echo '<div class=clause-profil>
-                    <h1>Mon profil</h1>
-                    <p>Nom : $NOM <br><br> Prénom : $prenom <br><br> Email : $mail <br><br></p>
-                    <form method="POST">
-                        <button type="submit" name="modifierProfil">MODIFIER MON PROFIL</button> <br><br>
-                    </form>
-                    <button>SUPPRIMER MON COMPTE</button> <br><br>
-                    <button>SE DECONNECTER</button>
-                </div>';
-        } else {
-            echo '<div class=clause-profil>
-                    <h1>Mon profil</h1>
-                    <p>Nom : $NOM <br><br> Prénom : $prenom <br><br> Email : $mail <br><br></p>
-                    <form method="POST">
-                        <button type="submit" name="modifierProfil">MODIFIER MON PROFIL</button> <br><br>
-                    </form>
-                    <button>SUPPRIMER MON COMPTE</button> <br><br>
-                    <button>SE DECONNECTER</button>
-                </div>';
-        }
-        ?>
+        </div>;
         
-
+        <?php include './views/partials/footer.php' ?>
     </main>
 
 </body>
