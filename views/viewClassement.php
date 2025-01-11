11c9327a3a7180a3aae9a5e16522d5b74c4a08ab
noctandc <maloucharan@gmail.com>
2025-01-11 13:25:48 +0100

    finlisation du classement

--- views/viewClassement.php
+++ views/viewClassement.php
@@ -5,10 +5,43 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Footer</title>
   <link rel="stylesheet" href="../assets/css/style.css">
+  <link rel="stylesheet" href="../assets/css/styleClassement.css"> 
 </head>
 <body>
-  <footer>
-    Game Collection - 2024 - Tous droits réservés
-  </footer>
+    <!-- HEADER -->
+
+    <?php include './partials/header.php'; ?>
+
+    <main>
+        <div class="bloc-classement">
+            <h1>Classement des temps passés</h1>
+            <table>
+                <tr class="niv1">
+                    <th>Joueur</th>
+                    <th>Temps passés</th>
+                    <th>Jeu favori</th>
+                </tr>
+                <tr class="niv1">
+                    <td>Nicolas</td>
+                    <td>120h</td>
+                    <td>Sybéria</td>
+                </tr>
+                <tr class="niv2">
+                    <td>Lou</td>
+                    <td>90h</td>
+                    <td>Genshin Impact</td>
+                </tr>
+                <tr class="niv1">
+                    <td>Justine</td>
+                    <td>300h</td>
+                    <td>Cell's to singularity</td>
+                </tr>
+            </table> 
+        </div>
+        <!-- FOOTER -->
+        <?php include './partials/footer.php'; ?>
+
+    </main>
+
 </body>
 </html>
